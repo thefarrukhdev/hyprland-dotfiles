140277f9f37649950285ddafa56d9a13184de97d
Farrukh-Front-Dev <farrukh.front.dev@gmail.com>
2026-09-25 18:07:52 +0500

    feat: add automatic package installer script

---
+++ install_packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Hyprland Dotfiles Package Installer
+echo "O'rnatish boshlanmoqda..."
+sudo pacman -S --needed hyprland waybar kitty rofi swaync cliphist grim slurp wl-clipboard hyprlock hypridle brightnessctl playerctl starship polkit-gnome xdg-desktop-portal-hyprland zsh zsh-autosuggestions zsh-syntax-highlighting ttf-firacode-nerd ttf-jetbrains-mono-nerd git
+echo "Barcha asosiy dasturlar o'rnatildi!"
